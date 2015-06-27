5dbbe6858a447532fb977c8fac798dd31f04f5ef
ThaoHa <havanthao93@gmail.com>
2015-06-27 16:03:39 +0700

    Remove save to db, add field directly

--- mb-wordpress-seo.php
+++ mb-wordpress-seo.php
@@ -8,69 +8,35 @@
  * Author URI: http://metabox.io
  */
 
-add_filter( 'rwmb_end_html', array( 'MB_WordPress_Seo', 'mb_html' ), 10, 3 );
-add_filter( 'rwmb_before_save_post', array( 'MB_WordPress_Seo', 'mb_save' ) );
-add_filter( 'wpseo_pre_analysis_post_content', array( 'MB_WordPress_Seo', 'mb_seo' ), 10, 2 );
+add_filter( 'wpseo_pre_analysis_post_content', array( 'MB_WordPress_Seo', 'add_custom_field' ) );
 
 if ( ! class_exists( 'MB_WordPress_Seo' ) )
 {
 	class MB_WordPress_Seo
 	{
 		/**
-		 * Add html
-		 *
-		 * @param $end
-		 * @param $field
-		 * @param $meta
-		 *
-		 * @return string
-		 */
-		static function mb_html( $end, $field, $meta )
-		{
-			if ( isset( $field['add_to_wpseo_analysis'] ) && $field['add_to_wpseo_analysis'] == true )
-			{
-				return $end . '<input type="hidden" name="mb_wp_seo[]" value="' . $field['id'] . '">';
-			}
-
-			return $end;
-		}
-
-		/**
-		 * Save to DB
-		 *
-		 * @param $postId
-		 */
-		static function mb_save( $postId )
-		{
-			$fields = isset( $_POST['mb_wp_seo'] ) ? $_POST['mb_wp_seo'] : array();
-			delete_post_meta( $postId, 'mb_wp_seo' );
-
-			foreach ( $fields as $field )
-			{
-				add_post_meta( $postId, 'mb_wp_seo', $field, false );
-			}
-		}
-
-		/**
 		 * Add field value to seo analysis
 		 *
 		 * @param $content
 		 */
-		static function mb_seo( $content, $post )
+		static function add_custom_field( $content )
 		{
-			global $post;
+			$meta_boxes = apply_filters( 'rwmb_meta_boxes', array() );
 
-			$pid = $post->ID;
-			$custom = get_post_custom( $pid );
-			$fields = ! empty( $custom['mb_wp_seo'] ) ? $custom['mb_wp_seo'] : array();
+		    foreach ( $meta_boxes as $meta_box )
+		    {
+		        foreach ( $meta_box['fields'] as $field )
+		        {
+		            if ( isset( $field['add_to_wpseo_analysis'] ) && $field['add_to_wpseo_analysis'] )
+		            {
+		            	$value = rwmb_meta( $field['id'], $field['type'] );
+		            	$value = is_array( $value ) ? implode( ' ', $value ) : $value;
+		                $content .= ' ' . $value;
+		            }
+		        }
+		    }
 
-			foreach ( $fields as $field )
-			{
-				$customContent = implode( ' ', $custom[$field] );
-				$content .= ' ' . $customContent;
-			}
-
-			return $content;
+		    return $content;
 		}
 	}
 }
